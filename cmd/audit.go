1c45120a5e781e2cc88ce15e6d7a44a39fb02f71
Davanum Srinivas <davanum@gmail.com>
2022-01-18 08:52:37 -0500

    better readable messages
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- cmd/audit.go
+++ cmd/audit.go
@@ -96,33 +96,33 @@ var auditCmd = &cobra.Command{
 
 func auditGroup(pwd string, groupType string, group utils.Group, context *utils.Context) {
 	if len(group.Dir) == 0 {
-		fmt.Printf("WARNING: missing 'dir' for a group under %s/%s\n", groupType, group.Name)
+		fmt.Printf("WARNING: missing 'dir' key\n")
 	}
 	if len(group.Name) == 0 {
-		fmt.Printf("WARNING: missing 'name' for a group under %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'name' key\n")
 	}
-	fmt.Printf(">>>> Processing %s [%s/%s]\n", groupType, group.Dir, group.Name)
+	fmt.Printf("\n>>>> Processing %s [%s/%s]\n", groupType, group.Dir, group.Name)
 	if len(group.MissionStatement) == 0 {
-		fmt.Printf("WARNING: missing 'mission_statement' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'mission_statement' key\n")
 	}
 	if len(group.CharterLink) == 0 {
-		fmt.Printf("WARNING: missing 'charter_link' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'charter_link' key\n")
 	} else {
-		auditCharterLink(pwd, groupType, group)
+		auditCharterLink(pwd, group)
 	}
 	if groupType == "workinggroups" {
 		auditWorkingGroupStakeholders(group, groupType, context)
 	}
 	if len(group.Label) == 0 {
-		fmt.Printf("WARNING: missing 'label' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'label' keys\n")
 	}
 	auditLeadership(group, groupType)
 	if len(group.Meetings) == 0 {
-		fmt.Printf("WARNING: missing 'meetings' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'meetings' key\n")
 	}
-	auditContact(&group.Contact, groupType, group)
+	auditContact(&group.Contact)
 	if len(group.Subprojects) == 0 {
-		fmt.Printf("WARNING: missing 'subprojects' for a group under %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'subprojects' key\n")
 	} else {
 		auditSubProject(group, groupType)
 	}
@@ -130,57 +130,58 @@ func auditGroup(pwd string, groupType string, group utils.Group, context *utils.
 
 func auditSubProject(group utils.Group, groupType string) {
 	for _, subproject := range group.Subprojects {
-		extra := fmt.Sprintf("[%s]", subproject.Name)
+		fmt.Printf("\n>>>> Processing subproject %s under %s\n", subproject.Name, group.Dir)
 		if len(subproject.Name) == 0 {
-			fmt.Printf("WARNING: missing name for subproject %s for a group under %s/%s\n", extra, groupType, group.Dir)
+			fmt.Printf("WARNING: missing 'name' key\n")
 		}
 		if len(subproject.Description) == 0 {
-			fmt.Printf("WARNING: missing description for subproject %s for a group under %s/%s\n", extra, groupType, group.Dir)
+			fmt.Printf("WARNING: missing 'description' key\n")
 		}
 		if subproject.Contact == nil {
-			fmt.Printf("WARNING: missing contact for subproject %s for a group under %s/%s\n", extra, groupType, group.Dir)
+			fmt.Printf("WARNING: missing 'contact' key\n")
 		} else {
-			auditContact(subproject.Contact, groupType, group)
+			auditContact(subproject.Contact)
 		}
 		if len(subproject.Owners) == 0 {
-			fmt.Printf("WARNING: missing owners for subproject %s for a group under %s/%s\n", extra, groupType, group.Dir)
+			fmt.Printf("WARNING: missing 'owners' key\n")
 		} else {
-			auditOwnersFiles(group, subproject, extra)
+			auditOwnersFiles(group, subproject)
 		}
 		if len(subproject.Meetings) == 0 {
-			fmt.Printf("WARNING: missing meetings for subproject %s for a group under %s/%s\n", extra, groupType, group.Dir)
+			fmt.Printf("WARNING: missing 'meetings' key\n")
 		}
 	}
 }
 
-func auditOwnersFiles(group utils.Group, subproject utils.Subproject, extra string) {
+func auditOwnersFiles(group utils.Group, subproject utils.Subproject) {
+	fmt.Printf("\n>>>> Processing owners files for %s/%s\n", group.Dir, subproject.Name)
 	for _, url := range subproject.Owners {
 		if strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://") {
 			resp, err := http.Get(url)
 			if err == nil && resp.StatusCode == 200 {
 				bytes, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-					fmt.Printf("ERROR: unable to read from owners file for subproject %s url [%s] %s\n", extra, url, err)
+					fmt.Printf("ERROR: unable to read owners file at %s url - %v\n", url, err)
 				}
 				info, err := utils.GetOwnersInfoFromBytes(bytes)
 				if err != nil {
-					fmt.Printf("ERROR: unable to parse from owners file for subproject %s url [%s] %s\n", extra, url, err)
+					fmt.Printf("ERROR: unable to parse owners file at %s url - %v\n", url, err)
 				} else {
 					if !strings.Contains(url, "kubernetes/kubernetes") {
 						continue
 					}
-					auditOwnersInfo(group, info, url, extra)
+					auditOwnersInfo(group, info, url)
 				}
 			} else {
-				fmt.Printf("WARNING: owners file for subproject %s  has a stale url [%s] http status code = %d %s\n", extra, url, resp.StatusCode, err)
+				fmt.Printf("WARNING: stale url %s - http status code = %d - %s\n", url, resp.StatusCode, err)
 			}
 		} else {
-			fmt.Printf("WARNING: owners file for subproject %s should be a url instead of [%s]\n", extra, url)
+			fmt.Printf("WARNING: owners file should be a url, found [%s]\n", url)
 		}
 	}
 }
 
-func auditOwnersInfo(group utils.Group, info *utils.OwnersInfo, url string, extra string) {
+func auditOwnersInfo(group utils.Group, info *utils.OwnersInfo, url string) {
 	if len(info.Labels) > 0 {
 		if len(group.Label) > 0 {
 			found := false
@@ -190,11 +191,11 @@ func auditOwnersInfo(group utils.Group, info *utils.OwnersInfo, url string, extr
 				}
 			}
 			if !found {
-				fmt.Printf("WARNING %s does not have a label that ends with %s. Please ensure OWNERS file has labels reflecting %s\n", url, group.Label, group.Dir)
+				fmt.Printf("WARNING: file does not have a label that contains with %s. Please ensure OWNERS file has labels reflecting %s - %s\n", group.Label, group.Dir, url)
 			}
 		}
 	} else {
-		fmt.Printf("WARNING: file at %s url for %s does not have any labels. Please ensure OWNERS file has labels reflecting %s\n", url, extra, group.Dir)
+		fmt.Printf("WARNING: file at url does not have any labels. Please ensure OWNERS file has labels reflecting %s - %s\n", group.Dir, url)
 	}
 	allOwners := []string{}
 	allOwners = append(allOwners, info.Approvers...)
@@ -207,48 +208,48 @@ func auditOwnersInfo(group utils.Group, info *utils.OwnersInfo, url string, extr
 		}
 	}
 	if !found {
-		fmt.Printf("WARNING: file at %s url does not seem to have approvers/reviewers with the " +
-			"sig alias (defined in OWNER_ALIASES). Please consider adding a sig alias OWNER_ALIASES and " +
-			"add them to approvers/reviewers in this file.\n", url)
+		fmt.Printf("WARNING: file at url does not seem to have approvers/reviewers with the "+
+			"sig alias (defined in OWNER_ALIASES). Please consider adding a sig alias OWNER_ALIASES and "+
+			"add them to approvers/reviewers in this file - %s\n", url)
 	}
 }
 
-func auditContact(contact *utils.Contact, groupType string, group utils.Group) {
+func auditContact(contact *utils.Contact) {
 	if len(contact.Slack) == 0 {
-		fmt.Printf("WARNING: missing 'slack' for %s/%s under contact\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'slack' in contact\n")
 	}
 	if len(contact.MailingList) == 0 {
-		fmt.Printf("WARNING: missing 'mailing_list' for %s/%s under contact\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'mailing_list' in contact\n")
 	}
 	if len(contact.PrivateMailingList) == 0 {
-		fmt.Printf("WARNING: missing 'private_mailing_list' for %s/%s under contact\n", groupType, group.Dir)
+		fmt.Printf("OPTIONAL: missing 'private_mailing_list' in contact\n")
 	}
 	if len(contact.GithubTeams) == 0 {
-		fmt.Printf("WARNING: missing 'teams' for %s/%s under contact\n", groupType, group.Dir)
+		fmt.Printf("OPTIONAL: missing 'teams' in contact\n")
 	}
 	if contact.Liaison != nil {
-		auditPerson(group, groupType, "contact/liaison", contact.Liaison)
+		auditPerson("contact/liaison", contact.Liaison)
 	}
 }
 
-func auditCharterLink(pwd string, groupType string, group utils.Group) {
+func auditCharterLink(pwd string, group utils.Group) {
 	if strings.HasPrefix(group.CharterLink, "http") {
 		client := &http.Client{}
 		resp, err := client.Get(group.CharterLink)
 		if err != nil || resp.StatusCode != http.StatusOK {
-			fmt.Printf("WARNING: unable to reach url %s for %s/%s\n", group.CharterLink, groupType, group.Dir)
+			fmt.Printf("WARNING: unable to reach url for 'charter_link' - %s\n", group.CharterLink)
 		}
 	} else {
 		charterPath := path.Join(pwd, group.Dir, group.CharterLink)
 		if _, err := os.Stat(charterPath); errors.Is(err, os.ErrNotExist) {
-			fmt.Printf("WARNING: missing file %s for %s/%s\n", charterPath, groupType, group.Dir)
+			fmt.Printf("WARNING: missing file for 'charter_link' - %s\n", charterPath)
 		}
 	}
 }
 
 func auditWorkingGroupStakeholders(group utils.Group, groupType string, context *utils.Context) {
 	if len(group.StakeholderSIGs) == 0 {
-		fmt.Printf("WARNING: missing 'stakeholder_sigs' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'stakeholder_sigs' key\n")
 	} else {
 		for _, stakeholder := range group.StakeholderSIGs {
 			found := false
@@ -259,7 +260,7 @@ func auditWorkingGroupStakeholders(group utils.Group, groupType string, context 
 				}
 			}
 			if !found {
-				fmt.Printf("WARNING: stakeholder_sigs entry '%s' not found for %s/%s\n", stakeholder, groupType, group.Dir)
+				fmt.Printf("WARNING: stakeholder_sigs entry '%s' not found (typo?)\n", stakeholder)
 			}
 		}
 	}
@@ -267,17 +268,17 @@ func auditWorkingGroupStakeholders(group utils.Group, groupType string, context 
 
 func auditLeadership(group utils.Group, groupType string) {
 	if len(group.Leadership.Chairs) == 0 {
-		fmt.Printf("WARNING: missing 'leadership/chairs' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'chairs' key (in 'leadership' section)\n")
 		if groupType == "sigs" {
 			if len(group.Leadership.Chairs) == 1 {
-				fmt.Printf("WARNING: please consider adding more folks in 'leadership/chairs' for %s/%s\n", groupType, group.Dir)
+				fmt.Printf("WARNING: please consider adding more folks in as 'chairs' (in 'leadership' section)\n")
 			}
 		}
 	}
 	if len(group.Leadership.TechnicalLeads) == 0 {
-		fmt.Printf("WARNING: missing 'leadership/tech_leads' for %s/%s\n", groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'tech_leads' key (in 'leadership' section)\n")
 		if groupType == "sigs" {
-			fmt.Printf("WARNING: if chairs are serving as tech leads, please add them explicitly in 'leadership/tech_leads' for %s/%s\n", groupType, group.Dir)
+			fmt.Printf("WARNING: if chairs are serving as tech leads, please add them explicitly in 'tech_leads' key (in 'leadership' section)\n")
 		}
 	}
 	var persons []utils.Person
@@ -285,18 +286,18 @@ func auditLeadership(group utils.Group, groupType string) {
 	persons = append(persons, group.Leadership.TechnicalLeads...)
 	persons = append(persons, group.Leadership.EmeritusLeads...)
 	for _, person := range persons {
-		auditPerson(group, groupType, "leadership", &person)
+		auditPerson("leadership", &person)
 	}
 }
 
-func auditPerson(group utils.Group, groupType string, extra string, person *utils.Person) {
+func auditPerson(extra string, person *utils.Person) {
 	if len(person.Name) == 0 {
-		fmt.Printf("WARNING: missing %s name for [%s] for %s/%s\n", extra, person.Name, groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'name' key in %s\n", extra)
 	}
 	if len(person.GitHub) == 0 {
-		fmt.Printf("WARNING: missing %s github id for [%s] for %s/%s\n", extra, person.Name, groupType, group.Dir)
+		fmt.Printf("WARNING: missing 'github' key in %s\n", extra)
 	}
 	if len(person.Company) == 0 {
-		fmt.Printf("WARNING: missing %s company for [%s] for %s/%s\n", extra, person.Name, groupType, group.Dir)
+		fmt.Printf("OPTIONAL: missing 'company' key in %s\n", extra)
 	}
 }
